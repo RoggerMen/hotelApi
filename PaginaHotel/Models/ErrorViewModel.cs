1ed0e51a51bfbc021c667e5362095a5b7598b75b
Rogger Meneses <roggermeneses984@correo.com>
2023-09-27 18:15:33 -0500

    Primer commit de api

---
+++ PaginaHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaginaHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
